f1a090f5e9108586be308a35bd1f416d38ee0b94
Luis <lomejordejr@riseup.net>
2016-03-14 16:17:46 -0500

    Agregado propiedad Documentos

--- src/com/generador/vista/Panel.java
+++ src/com/generador/vista/Panel.java
@@ -4,6 +4,10 @@ import java.util.Observer;
 
 import javax.swing.JPanel;
 
+import org.jsoup.nodes.Document;
+
+import com.generador.controlador.Documentos;
+
 /**
  * 
  * Clase abstracta encargada de implementar los paneles de
@@ -14,7 +18,17 @@ import javax.swing.JPanel;
  */
 
 public abstract class Panel implements Observer{
-
+	
 	protected abstract JPanel getPanel();
 	protected abstract void setContenido();
+	
+	public void setDocMateriasPosibles(Document docMateriasPosibles) {
+		Documentos documentos = Documentos.instancia();
+		documentos.setDocMateriasPosibles(docMateriasPosibles);
+	}
+	
+	public void setDocHorarioMaterias(Document docHorarioMaterias) {
+		Documentos documentos = Documentos.instancia();
+		documentos.setDocHorarioMaterias(docHorarioMaterias);
+	}	
 }
