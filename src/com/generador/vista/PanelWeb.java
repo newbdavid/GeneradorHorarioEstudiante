daab5eb1e794b94381c822980729b0bf7b36a557
Luis <lomejordejr@riseup.net>
2016-03-14 18:18:01 -0500

    Agregado login a PanelWeb

--- src/com/generador/vista/PanelWeb.java
+++ src/com/generador/vista/PanelWeb.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Observable;
 
 import javax.swing.JButton;
@@ -13,8 +14,20 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.generador.modelo.Cookies;
 import com.generador.utilidad.JTextFieldLimit;
 
 /**
@@ -31,6 +44,7 @@ public class PanelWeb extends Panel {
 	private JLabel lblCedula, lblPass, lblPeriodo;
 	private JTextField txtCedula, txtPass;
 	private JComboBox<String> cboPeriodo;
+	private Cookies cookie;
 	
 	public PanelWeb() {
 		panelWeb = new JPanel();
@@ -38,16 +52,80 @@ public class PanelWeb extends Panel {
 		panelWeb.setSize(500, 480);
 		setContenido();
 		
+		cookie = Cookies.instancia();
+		
 		//Listener
 		btnConectar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-				JOptionPane.showMessageDialog(null, "Test");
+				
+				if (login()) {
+					JOptionPane.showMessageDialog(null, "Test");
+					setDocHorarioMaterias(getDocHorarioMaterias());
+					setDocMateriasPosibles(getDocMateriasPosibles());
+				} else {
+					JOptionPane.showMessageDialog(null, "Error");
+				}
 			}
 		});
 		
+	}
+	
+	public boolean login() {
+		
+		//Configuracion webClient
+		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_38);
+		webClient.getCookieManager().setCookiesEnabled(true);
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+
+		HtmlPage page;
+		
+		try {
+			page = webClient.getPage("https://saew.epn.edu.ec");
+			HtmlForm form = page.getFormByName("aspnetForm");
+
+			HtmlSelect selectUser = form.getSelectByName("ctl00$ContentPlaceHolder1$cmbmodo");
+			selectUser.setSelectedIndex(3);
+			webClient.waitForBackgroundJavaScript(3000);
+
+			form = page.getFormByName("aspnetForm");
+
+			HtmlTextInput inputCedula = form.getInputByName("ctl00$ContentPlaceHolder1$UserName");
+			
+			//Ingresar Usuario
+			inputCedula.setText(txtCedula.getText());
+			webClient.waitForBackgroundJavaScript(3000);
+
+			HtmlPasswordInput inputPassword = form.getInputByName("ctl00$ContentPlaceHolder1$Password");
+			
+			//Ingresar Password
+			inputPassword.setText(txtPass.getText());
+			webClient.waitForBackgroundJavaScript(100);
+
+			HtmlSubmitInput btnIngresar = form.getInputByName("ctl00$ContentPlaceHolder1$LoginButton");
+			page = btnIngresar.click();
+			webClient.waitForBackgroundJavaScript(100);
+
+			form = page.getFormByName("aspnetForm");
+			selectUser = form.getSelectByName("ctl00$ContentPlaceHolder1$cmbperiodo");
+			
+			//Seleccionar periodo
+			selectUser.setSelectedIndex(cboPeriodo.getSelectedIndex());
+			btnIngresar = form.getInputByName("ctl00$ContentPlaceHolder1$LoginButton");
+			
+			btnIngresar.click();
+			webClient.waitForBackgroundJavaScript(100);
+
+			page = btnIngresar.click();
+			cookie.setStrCookies(webClient.getCookieManager().getCookie("ASP.NET_SessionId").getValue());
+			
+		} catch (FailingHttpStatusCodeException | IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error: Conexión no establecida");
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
@@ -79,8 +157,8 @@ public class PanelWeb extends Panel {
 		
 		cboPeriodo = new JComboBox<>();
 		cboPeriodo.addItem("2016-A");
-		cboPeriodo.addItem("2016-B");
-		cboPeriodo.addItem("2016-C");
+		cboPeriodo.addItem("2015-R");
+		cboPeriodo.addItem("2015-B");
 		
 		btnConectar = new JButton("Conectar");
 		
@@ -96,6 +174,35 @@ public class PanelWeb extends Panel {
 		panelWeb.add(btnConectar, BorderLayout.CENTER);
 	}
 	
+	public Document getDocMateriasPosibles() {
+		
+		Document materiasPosibles = null;
+		try {
+			materiasPosibles = Jsoup.connect("https://saew.epn.edu.ec/SAEINF/MateriasPosibles.aspx")
+					.cookie("ASP.NET_SessionId", cookie.getStrCookies())
+					.get();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error: Obtener materias posibles");
+		}
+		
+		return materiasPosibles;
+	}
+	
+	public Document getDocHorarioMaterias() {
+		
+		Document horarios = null;
+		try {
+			horarios = Jsoup.connect("https://saew.epn.edu.ec/SAEINF/HorariosMaterias.aspx")
+					.cookie("ASP.NET_SessionId", cookie.getStrCookies())
+					.get();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"Error: Obtener materias posibles");
+		}
+		
+		return horarios;
+	}
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
