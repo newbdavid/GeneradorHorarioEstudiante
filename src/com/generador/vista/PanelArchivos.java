03e63a99b26bab065dea3ca5dad54c3ba47cca54
Luis <lomejordejr@riseup.net>
2016-03-14 16:15:51 -0500

    FileChooser actualizado

--- src/com/generador/vista/PanelArchivos.java
+++ src/com/generador/vista/PanelArchivos.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 
 import javax.swing.JButton;
@@ -12,16 +13,15 @@ import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.jsoup.Jsoup;
 
 public class PanelArchivos extends Panel {
 
 	private JPanel panelArchivosLocales;
-	private JLabel labelLocalMateriasPosibles;
-	private JLabel labelPathMateriaPosibles;
-	private JButton btnMateriasPosibles;
-	private JLabel labelLocalHorarios;
-	private JLabel labelPathHorarios;
-	private JButton btnHorarios;
+	private JLabel lblLocalMateriasPosibles, lblPathMateriaPosibles, lblLocalHorarios, lblPathHorarios;
+	private JButton btnMateriasPosibles, btnHorarios;
 	
 	public PanelArchivos() {
 		panelArchivosLocales = new JPanel(new FlowLayout(FlowLayout.TRAILING));
@@ -32,21 +32,23 @@ public class PanelArchivos extends Panel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser fileMatariasPosibles = new JFileChooser();
-				int result = fileMatariasPosibles.showOpenDialog(null);
-				if (result == JFileChooser.APPROVE_OPTION) {
-					File selectedFile = fileMatariasPosibles.getSelectedFile();
-
-					if (selectedFile.getAbsolutePath().matches(".*Materias Posibles\\.html$")) {
-						//setStrPathMateriasPosibles(selectedFile.getAbsolutePath());
-						labelPathMateriaPosibles.setText(selectedFile.getAbsolutePath());
+				
+				File fileMaterias = selector();
+				if (fileMaterias != null) {
+					if (fileMaterias.getAbsolutePath().matches(".*Materias Posibles\\.html$")) {
+						try {
+							setDocMateriasPosibles(Jsoup.parse(fileMaterias, "UTF-8", ""));
+						} catch (IOException e1) {
+							e1.printStackTrace();
+							JOptionPane.showMessageDialog(null,"Conversión: Materias Posibles invalido\n");
+						}
+						lblPathMateriaPosibles.setText(fileMaterias.getAbsolutePath());
 					} else {
 						JOptionPane.showMessageDialog(null,
-								"Archivo: Materias Posibles invalido\n"
-						+ "Seleccionar archivo Materias Posibles.html");
+								"Archivo: Materias Posibles invalido\nSeleccionar archivo Materias Posibles.html");
 					}
-
 				}
+				
 			}
 		});
 
@@ -54,47 +56,63 @@ public class PanelArchivos extends Panel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser fileHorarios = new JFileChooser();
-				int result = fileHorarios.showOpenDialog(null);
-				if (result == JFileChooser.APPROVE_OPTION) {
-					File selectedFile = fileHorarios.getSelectedFile();
-
-					if (selectedFile.getAbsolutePath().matches(".*Horarios\\.html$")) {
-						//setStrPathHorarios(selectedFile.getAbsolutePath());
-						labelPathHorarios.setText(selectedFile.getAbsolutePath());
+				
+				File fileHorarios = selector();
+				if (fileHorarios != null){
+					if (fileHorarios.getAbsolutePath().matches(".*Horarios\\.html$")) {
+						try {
+							setDocHorarioMaterias(Jsoup.parse(fileHorarios, "UTF-8", ""));
+						} catch (IOException e1) {
+							e1.printStackTrace();
+							JOptionPane.showMessageDialog(null,"Conversión: Horarios Materias invalido\n");
+						}
+						lblPathHorarios.setText(fileHorarios.getAbsolutePath());
 					} else {
 						JOptionPane.showMessageDialog(null,
-								"Archivo: Horarios invalido\n"
-										+ "Seleccionar archivo Horarios.html");
+								"Archivo: Horarios invalido\n Seleccionar archivo Horarios.html");
 					}
 				}
 			}
 		});
 	}
 	
+	private File selector() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fileChooser.setMultiSelectionEnabled(false);
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		fileChooser.setFileFilter(new FileNameExtensionFilter("Archivo HTML", "HTML"));
+		
+		int result = fileChooser.showOpenDialog(null);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				return fileChooser.getSelectedFile();
+		}
+		return null;
+	}
+	
 	@Override
 	protected void setContenido() {
-		labelLocalMateriasPosibles = new JLabel("Archivo - Materias Posibles");
-		labelPathMateriaPosibles = new JLabel("");
-		labelLocalMateriasPosibles.setHorizontalTextPosition(JLabel.CENTER);
+		lblLocalMateriasPosibles = new JLabel("Archivo - Materias Posibles");
+		lblPathMateriaPosibles = new JLabel("");
+		lblLocalMateriasPosibles.setHorizontalTextPosition(JLabel.CENTER);
 		btnMateriasPosibles = new JButton("Seleccionar");
 
-		labelLocalHorarios = new JLabel("Archivo - Horarios");
-		labelPathHorarios = new JLabel("");
-		labelLocalHorarios.setHorizontalTextPosition(JLabel.CENTER);
+		lblLocalHorarios = new JLabel("Archivo - Horarios");
+		lblPathHorarios = new JLabel("");
+		lblLocalHorarios.setHorizontalTextPosition(JLabel.CENTER);
 		btnHorarios = new JButton("Seleccionar");
 
-		panelArchivosLocales.add(labelLocalMateriasPosibles,BorderLayout.CENTER);
+		panelArchivosLocales.add(lblLocalMateriasPosibles,BorderLayout.CENTER);
 		panelArchivosLocales.add(new JLabel("                   "),BorderLayout.CENTER);
 		panelArchivosLocales.add(btnMateriasPosibles, BorderLayout.CENTER);
-		panelArchivosLocales.add(labelPathMateriaPosibles, BorderLayout.CENTER);
+		panelArchivosLocales.add(lblPathMateriaPosibles, BorderLayout.CENTER);
 		panelArchivosLocales.add(new JLabel(
 				"                                                                 "
 				+ "                              "),BorderLayout.CENTER);
-		panelArchivosLocales.add(labelLocalHorarios, BorderLayout.CENTER);
+		panelArchivosLocales.add(lblLocalHorarios, BorderLayout.CENTER);
 		panelArchivosLocales.add(new JLabel("                   "),BorderLayout.CENTER);
 		panelArchivosLocales.add(btnHorarios, BorderLayout.CENTER);
-		panelArchivosLocales.add(labelPathHorarios, BorderLayout.CENTER);
+		panelArchivosLocales.add(lblPathHorarios, BorderLayout.CENTER);
 	}
 	
 	@Override
