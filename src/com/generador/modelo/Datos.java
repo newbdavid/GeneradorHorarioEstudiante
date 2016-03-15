14e1ad19ce2e69a0a706e81533806bbc30556998
Luis <lomejordejr@riseup.net>
2016-03-15 15:55:53 -0500

    Refactor clase Datos

--- src/com/generador/modelo/Datos.java
+++ src/com/generador/modelo/Datos.java
@@ -1,11 +1,8 @@
 package com.generador.modelo;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -32,83 +29,10 @@ public class Datos {
 	 * Constructor de la clase Datos
 	 * 
 	 * @param materiasPosibles
-	 *            dato materias posibles
+	 *            datos materias posibles
 	 * @param horariosMaterias
-	 *            dato horarios materias
-	 * @throws IOException
+	 *            datos horarios materias
 	 */
-	public Datos(File materiasPosibles, File horariosMaterias)
-			throws IOException {
-
-		Element materia;
-		Elements infoMateria;
-		Materia clase;
-
-		// Materias Posibles
-		this.docMateriasPosibles = Jsoup.parse(materiasPosibles, "UTF-8", "");
-		Elements tablaMateriasPosibles = docMateriasPosibles
-				.getElementsByClass("controlgrilla").get(0).children().get(0)
-				.children();
-
-		// Horario Materias
-		this.docHorarioMaterias = Jsoup.parse(horariosMaterias, "UTF-8", "");
-		Elements tablaHorarioMaterias = docHorarioMaterias
-				.getElementsByClass("controlgrilla").get(0).children().get(0)
-				.children();
-
-		// Almacenamiento Materias Posibles
-		MultiMapa mapTablaMateriasPosibles = new MultiMapa();
-		for (int i = 1; i < tablaMateriasPosibles.size() - 1; i++) {
-			materia = tablaMateriasPosibles.get(i);
-			infoMateria = materia.children();
-
-			// Almacenamiento horarios disponibles de cada posible materia
-			for (int j = 0; j < tablaHorarioMaterias.select(
-					"tr:contains(" + infoMateria.get(1).text() + ")").size(); j++) {
-
-				clase = new Materia(infoMateria.get(1).text(), infoMateria.get(
-						2).text(), infoMateria.get(3).text(), infoMateria
-						.get(4).text(), infoMateria.get(5).text(), infoMateria
-						.get(6).text());
-
-				// Información obtenida del archivo docHorariosMaterias
-				clase.setStrParalelo(tablaHorarioMaterias
-						.select("tr:contains(" + infoMateria.get(1).text()
-								+ ")").get(j).children().get(3).text());
-				clase.setStrAula(tablaHorarioMaterias
-						.select("tr:contains(" + infoMateria.get(1).text()
-								+ ")").get(j).children().get(4).text());
-				// Agrega los horarios a cada materia que tenga dicha
-				// informacion en docHorariosMaterias
-				clase.setHorario(new Horario(tablaHorarioMaterias
-						.select("tr:contains(" + infoMateria.get(1).text()
-								+ ")").get(j).children().get(5).text(),
-						tablaHorarioMaterias
-								.select("tr:contains("
-										+ infoMateria.get(1).text() + ")")
-								.get(j).children().get(6).text(),
-						tablaHorarioMaterias
-								.select("tr:contains("
-										+ infoMateria.get(1).text() + ")")
-								.get(j).children().get(7).text(),
-						tablaHorarioMaterias
-								.select("tr:contains("
-										+ infoMateria.get(1).text() + ")")
-								.get(j).children().get(8).text(),
-						tablaHorarioMaterias
-								.select("tr:contains("
-										+ infoMateria.get(1).text() + ")")
-								.get(j).children().get(9).text(),
-						tablaHorarioMaterias
-								.select("tr:contains("
-										+ infoMateria.get(1).text() + ")")
-								.get(j).children().get(10).text()));
-
-				mapTablaMateriasPosibles.put(infoMateria.get(1).text(), clase);
-			}
-		}
-		this.mapMaterias = mapTablaMateriasPosibles;
-	}
 
 	public Datos(Document materiasPosibles, Document horariosMaterias) {
 		Element materia;
