c4362fcf7b77cac9e6ead5b7149dcd9e41ce9e94
Luis <lomejordejr@riseup.net>
2016-03-17 09:52:11 -0500

    Agregado funcion restriccion repeticion

--- src/com/generador/controlador/FitnessHorario.java
+++ src/com/generador/controlador/FitnessHorario.java
@@ -1,7 +1,9 @@
 package com.generador.controlador;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.bcel.generic.RETURN;
 import org.jgap.FitnessFunction;
 import org.jgap.IChromosome;
 
@@ -11,6 +13,7 @@ import com.generador.utilidad.MultiMapa;
 public class FitnessHorario extends FitnessFunction {
 
 	private List<Materia> listaMaterias;
+	private List<Materia> auxOptimoMaterias;
 	
 	public FitnessHorario(List<Materia> listaMaterias) {
 		this.listaMaterias = listaMaterias;
@@ -21,30 +24,40 @@ public class FitnessHorario extends FitnessFunction {
 		Double valor = 0.0;
 		Integer creditos = 0;
 		Integer restriccion = 1;
+		auxOptimoMaterias = new ArrayList<Materia>();
 		for (int i = 0; i < arg0.size(); i++) {
 			
 			//Se considera si es verdadero
 			if ((Boolean)arg0.getGene(i).getAllele()) {
-				valor += valor(i,listaMaterias.get(i));
+				
+				if (isRepetido(listaMaterias.get(i))){
+					restriccion = 0;
+				}
+				
+				auxOptimoMaterias.add(listaMaterias.get(i));
+				valor += listaMaterias.get(i).getIntPrioridad();
 				creditos += listaMaterias.get(i).getIntCreditos();
 				
 				//Materias obligatorias
 				if (listaMaterias.get(i).getCategoria().getStrCategoria().equals("OBLIGATORIAS")){
-					valor += 200;
+					valor += 1000;
 				}
 				
 				//Restriccion creditos
 				if (creditos > 30) {
 					restriccion = 0;
 				}
-			} else {
-					
 			}
 		}
 		return valor * restriccion;	
 	}
+	
+	protected Boolean isRepetido (Materia materia) {
 		
-	protected double valor(int indice, Materia materia) {
-		return (double) materia.getIntPrioridad();
+		for (int i = 0; i < auxOptimoMaterias.size(); i++) {
+			if (auxOptimoMaterias.get(i).getStrCodigo().equals(materia.getStrCodigo()))
+					return true;
+		}
+		return false;
 	}
 }
