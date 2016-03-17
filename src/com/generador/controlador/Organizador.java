4b76ff6b97c79aab626c961b6bf8d24f7c0007d4
Luis <lomejordejr@riseup.net>
2016-03-17 12:14:31 -0500

    Agregado lista de soluciones

--- src/com/generador/controlador/Organizador.java
+++ src/com/generador/controlador/Organizador.java
@@ -1,5 +1,6 @@
 package com.generador.controlador;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jgap.Chromosome;
@@ -23,20 +24,25 @@ import com.generador.utilidad.MultiMapa;
  */
 
 public class Organizador {
-	
+
 	private List<Materia> listaMaterias;
+	private List<List<Materia>> soluciones;
+	private List<IChromosome> listaCromosomas;
 	private int numeroMaterias;
 	
+
 	public Organizador(MultiMapa mapMaterias) {
 		this.listaMaterias = mapMaterias.getAllMaterias();
 		this.numeroMaterias = this.listaMaterias.size();
+		this.soluciones = new ArrayList<List<Materia>>(5);
+		this.listaCromosomas = new ArrayList<IChromosome>();
 	}
-	
+
 	public void calcularHorarioOptimo () {
-		
+
 		//Configuracion predeterminada
 		Configuration config = new DefaultConfiguration();
-		
+
 		//Almacenar mejor cada generacion
 		config.setPreservFittestIndividual(true);
 
@@ -46,50 +52,65 @@ public class Organizador {
 			config.setFitnessFunction(fitness);
 
 			//Gen de ejemplo
-			
+
 			Gene[] genHorario = new Gene[numeroMaterias];
 			for (int i = 0; i < numeroMaterias; i++) {
 				genHorario[i] = new BooleanGene(config,false);	
 			}
-			
+
 			IChromosome cromosoma = new Chromosome(config, genHorario);
 			config.setSampleChromosome(cromosoma);
-			config.setPopulationSize(200);
-			
+			config.setPopulationSize(300);
+
 			Genotype poblacion;
 			poblacion = Genotype.randomInitialGenotype(config);
-			
+
+			IChromosome cromosomaOptimo;
+
 			//Evolucion
-			for (int i = 0; i < 2000; i++) {
-				poblacion.evolve();	
-			}
-			
-			IChromosome cromosomaOptimo = poblacion.getFittestChromosome();
-			resultado(cromosomaOptimo);
-			
+			for (int i = 0; i < 2500; i++) {
+				poblacion.evolve();
+				cromosomaOptimo = poblacion.getFittestChromosome();
+				if (cromosomaOptimo.getFitnessValue() > 700 && !listaCromosomas.contains(cromosomaOptimo))
+				{
+					listaCromosomas.add(cromosomaOptimo);
+					System.out.println(cromosomaOptimo.toString());
+					soluciones.add(resultado(cromosomaOptimo));
+				}
+			}	
 		} catch (InvalidConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		System.out.println("Soluciones obtenidas: "+soluciones.size());
 	}
-	
+
 	public String infoMateria (int indice) {
 		return listaMaterias.get(indice).toString();
 	}
-	
-	public void resultado (IChromosome cromosomaOptimo) {
-		
+
+	public List<Materia> resultado (IChromosome cromosomaOptimo) {
+
+		List<Materia> solucion = new ArrayList<Materia>(8);
+
+		for (int i = 0; i < numeroMaterias; i++) {
+			if ((boolean) cromosomaOptimo.getGene(i).getAllele()){
+				solucion.add(listaMaterias.get(i));
+			}
+		}
+
 		Integer creditos = 0;
 		System.out.println("valor:"+cromosomaOptimo.getFitnessValue());
-		System.out.println("edad: "+cromosomaOptimo.getAge());
+		
 		for (int i = 0; i < numeroMaterias; i++) {
-			System.out.println("gen"+i+":"+cromosomaOptimo.getGene(i).getAllele());
 			if ((boolean) cromosomaOptimo.getGene(i).getAllele()) {
 				System.out.println(infoMateria(i));
 				creditos += listaMaterias.get(i).getIntCreditos();
 			}
 		}
-		System.out.println("creditos total: "+creditos);
 		
+		System.out.println("creditos total: "+creditos);
+		return solucion;
 	}
 }
