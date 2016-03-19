3105238e0a2724b08795cd30127b4c863fa7aa66
Luis <lomejordejr@riseup.net>
2016-03-19 18:11:47 -0500

    Agregado checkbox en jtable todas las materias

--- src/com/generador/vista/PanelConfiguracion.java
+++ src/com/generador/vista/PanelConfiguracion.java
@@ -3,7 +3,8 @@ package com.generador.vista;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -27,82 +28,108 @@ public class PanelConfiguracion implements Observer {
 
 	JPanel panelConfiguracion;
 	JLabel lblMinCreditos, lblMaxCreditos;
-	JSpinner jspMinCreditos, jspMaxCreditos;
+	JSpinner spMinCreditos, spMaxCreditos;
 	JTable tblTodosHorarios;
 	JButton btnGuardar;
 	List<Materia> listaMaterias;
+	List <Integer> listaSeleccionado;
 	int minCreditos, maxCreditos;
-	
+
 	public PanelConfiguracion(List<Materia> listaMaterias) {
 		panelConfiguracion = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		setContenido(listaMaterias);
 		this.listaMaterias = listaMaterias;
-		
+
 		//Eventos
 		tblTodosHorarios.addMouseListener(new MouseAdapter() {
-		    public void mousePressed(MouseEvent me) {
-		        int fila = tblTodosHorarios.rowAtPoint(me.getPoint());
-		        if (me.getClickCount() == 2 && fila > -1 ) {
-		        	int respuesta = JOptionPane.showConfirmDialog(null, "¿Desea elimnar la materia selecionada?"
-		        			, "Confirmación", JOptionPane.YES_NO_OPTION);
-		        	 if (respuesta == JOptionPane.YES_OPTION) {
-		        		 listaMaterias.remove(fila);
-		        		 ((DefaultTableModel)tblTodosHorarios.getModel()).removeRow(fila);
-		        	 }
-		        }
-		    }
+			public void mousePressed(MouseEvent me) {
+				int fila = tblTodosHorarios.rowAtPoint(me.getPoint());
+				if (me.getClickCount() == 2 && fila > -1 ) {
+					int respuesta = JOptionPane.showConfirmDialog(null, "¿Desea elimnar la materia selecionada?"
+							, "Confirmación", JOptionPane.YES_NO_OPTION);
+					if (respuesta == JOptionPane.YES_OPTION) {
+						listaMaterias.remove(fila);
+						((DefaultTableModel)tblTodosHorarios.getModel()).removeRow(fila);
+					}
+				}
+			}
 		});
 	}
 
 	protected void setContenido(List<Materia> listaMaterias) {
 		lblMinCreditos = new JLabel("Minimo creditos");
 		lblMinCreditos.setHorizontalTextPosition(JLabel.CENTER);
-		jspMinCreditos = new JSpinner(new SpinnerNumberModel(15, 0, 30, 1));
+		spMinCreditos = new JSpinner(new SpinnerNumberModel(15, 0, 30, 1));
 
 		lblMaxCreditos = new JLabel("Máximo creditos");
 		lblMaxCreditos.setHorizontalTextPosition(JLabel.CENTER);
-		jspMaxCreditos = new JSpinner(new SpinnerNumberModel(30, 0, 30, 1)); 
-		
+		spMaxCreditos = new JSpinner(new SpinnerNumberModel(30, 0, 30, 1)); 
+
 		String[] colName = { "Codigo", "Nombre", "Paralelo", "Aula", "Horario",
 				"Creditos", "Num Matricua", "Categoria", "Prioridad" };
+
+		//JTable no editable
 		DefaultTableModel tableModel = new DefaultTableModel() {
 			@Override
 			public boolean isCellEditable(int row, int column) {
-				return false;
+				if (column == tblTodosHorarios.getColumn("Obligatorio").getModelIndex())
+					return true;
+				else
+					return false;
 			}
 		};
 		tableModel.setColumnIdentifiers(colName);
-		tblTodosHorarios = new JTable(tableModel);
+		tblTodosHorarios = new JTable(tableModel) {
+			//Agregado checkbox
+			@Override
+            public Class getColumnClass(int column) {
+                switch (column) {
+                    case 9:
+                        return Boolean.class;
+                    default:
+                        return String.class;
+                }
+            }
+		};
 
+		//Contenido Jtable
 		Object[] datos = new String[9];
 		for (Materia materia : listaMaterias) {
 			System.out.println(materia.getInfoMateria().toString());
 			datos = materia.getInfoMateria();
 			tableModel.addRow(datos);
 		}
+
+		Object[] checkColumn = new Boolean[listaMaterias.size()];
+		for (int i = 0; i < listaMaterias.size(); i++) {
+			checkColumn[i] = false;
+		}	
+		tableModel.addColumn("Obligatorio", checkColumn);
+
 		tblTodosHorarios.setModel(tableModel);
 		tblTodosHorarios.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		tblTodosHorarios.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		JScrollPane scrollTabla = new JScrollPane(tblTodosHorarios);
 		scrollTabla.setPreferredSize(new Dimension(480, 300));
+
 		tblTodosHorarios.getColumnModel().getColumn(1).setPreferredWidth(300);
 		tblTodosHorarios.getColumnModel().getColumn(4).setPreferredWidth(300);
-		
+		tblTodosHorarios.setFillsViewportHeight(true);
 		btnGuardar = new JButton("Guardar");
-		
+
 		panelConfiguracion.add(lblMinCreditos, BorderLayout.CENTER);
-		panelConfiguracion.add(jspMinCreditos, BorderLayout.CENTER);
+		panelConfiguracion.add(spMinCreditos, BorderLayout.CENTER);
 		panelConfiguracion.add(lblMaxCreditos, BorderLayout.CENTER);
-		panelConfiguracion.add(jspMaxCreditos, BorderLayout.CENTER);
+		panelConfiguracion.add(spMaxCreditos, BorderLayout.CENTER);
 		panelConfiguracion.add(scrollTabla, BorderLayout.CENTER);
 		panelConfiguracion.add(btnGuardar, BorderLayout.CENTER);
 	}
-	
+
 	public JPanel getPanelConfiguracion() {
 		return panelConfiguracion;
 	}
-	
+
 	public int getMinCreditos() {
 		return minCreditos;
 	}
@@ -114,6 +141,6 @@ public class PanelConfiguracion implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
