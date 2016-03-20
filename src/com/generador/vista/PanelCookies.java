fb1f937cc5bffd01ec1a534f5524c3ecffb564e1
Luis <lomejordejr@riseup.net>
2016-03-20 10:08:45 -0500

    Agregado funcionalidad validador Carrera

--- src/com/generador/vista/PanelCookies.java
+++ src/com/generador/vista/PanelCookies.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Observable;
 
 import javax.swing.JButton;
@@ -16,6 +17,14 @@ import javax.swing.JPanel;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 import com.generador.modelo.Cookies;
 import com.generador.utilidad.Formateador;
 
@@ -33,17 +42,17 @@ public class PanelCookies extends Panel {
 	private JLabel lblCookies;
 	private JFormattedTextField txtCookies;
 	private JButton btnAceptar;
-	
+
 	public PanelCookies() {
 		panelCookies = new JPanel();
 		panelCookies.setLayout(new FlowLayout(FlowLayout.CENTER));
 		panelCookies.setSize(500, 480);
 		setContenido();
-		
+
 		//Observer
 		cookie = Cookies.instancia();
 		cookie.addObserver(this);
-		
+
 		//Listener
 		btnAceptar.addActionListener(new ActionListener() {
 
@@ -51,18 +60,53 @@ public class PanelCookies extends Panel {
 			public void actionPerformed(ActionEvent e) {
 				if (txtCookies.getText().length() == 24) {
 					cookie.setStrCookies(txtCookies.getText());
+					autenticacionCarrera();
 					setDocHorarioMaterias(getDocHorarioMaterias());
 					setDocMateriasPosibles(getDocMateriasPosibles());
-					
+
 				} else {
 					JOptionPane.showMessageDialog(null,"Cookie de Sesión inválido");
 				}
 			}
 		});
 	}
-	
+
+	public void autenticacionCarrera () {
+		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_38);
+		Cookie cookies = new Cookie("saew.epn.edu.ec", "ASP.NET_SessionId", cookie.getStrCookies());
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+
+		HtmlPage page;
+		HtmlForm form;
+		HtmlSelect selectUser;
+
+		try {
+			//Autenticación carrera
+			webClient.getCookieManager().addCookie(cookies);
+			page = webClient.getPage("https://saew.epn.edu.ec/SAEINF/MateriasPosibles.aspx");
+			form = page.getFormByName("aspnetForm");
+			
+			if (page.getElementById("ctl00_ContentPlaceHolder1_lblMensajeError").getTextContent()
+					.equalsIgnoreCase("Seleccione una Carrera")) {
+
+				selectUser = form.getSelectByName("ctl00$ContentPlaceHolder1$cmbCarrera");
+				//Selecionado primera opcion de carrera
+				selectUser.setSelectedIndex(0);
+				webClient.waitForBackgroundJavaScript(1000);
+			}
+
+		} catch (FailingHttpStatusCodeException e1) {
+			JOptionPane.showMessageDialog(null, "Error:\nConexión no establecida");
+			e1.printStackTrace();
+		} catch (MalformedURLException e1) { 
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
 	public Document getDocMateriasPosibles() {
-		
+
 		Document materiasPosibles = null;
 		try {
 			materiasPosibles = Jsoup.connect("https://saew.epn.edu.ec/SAEINF/MateriasPosibles.aspx")
@@ -72,12 +116,12 @@ public class PanelCookies extends Panel {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null,"Error: Obtener materias posibles");
 		}
-		
+
 		return materiasPosibles;
 	}
-	
+
 	public Document getDocHorarioMaterias() {
-		
+
 		Document horarios = null;
 		try {
 			horarios = Jsoup.connect("https://saew.epn.edu.ec/SAEINF/HorariosMaterias.aspx")
@@ -87,10 +131,10 @@ public class PanelCookies extends Panel {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null,"Error: Obtener materias posibles");
 		}
-		
+
 		return horarios;
 	}
-	
+
 	@Override
 	public void setContenido() {
 		lblCookies = new JLabel("Ingresar Cookies de Sesión");
@@ -107,7 +151,7 @@ public class PanelCookies extends Panel {
 		panelCookies.add(txtCookies, BorderLayout.CENTER);
 		panelCookies.add(btnAceptar, BorderLayout.CENTER);
 	}
-	
+
 	@Override
 	public JPanel getPanel() {
 		return panelCookies;
